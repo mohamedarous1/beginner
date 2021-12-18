647e0bd4cbd9802efa4d6b20d723c84eed29eef4
mohamedarous1 <81327373+mohamedarous1@users.noreply.github.com>
2021-12-18 23:35:28 +0200

    Add files via upload

---
+++ beginner/main.c
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+#define M_PI 3.14159265358979323846
+int main(){
+    int x = 653354;
+    int mod = pow(10,1);
+    int r = x %mod;
+    printf("%d",r);
+    return 0;
+
+}
